4de13658c72f1ba75b7681a52f22950f33a754b0
Arnaud SAULOU <a.saulou@gmail.com>
2018-05-07 11:33:49 +0200

    Récupération Story 2

--- src/main/java/SpaceInvaders.java
+++ src/main/java/SpaceInvaders.java
@@ -1,22 +1,50 @@
 public class SpaceInvaders {
 
+    public static final char MARQUE = 'V';
+    public static final char MARQUE_VIDE = '.';
+    public static final char MARQUE_FIN_LIGNE = '\n';
+
     int longueur;
     int hauteur;
+    Vaisseau vaisseau;
 
     public SpaceInvaders(int longueur, int hauteur) {
         this.longueur = longueur;
         this.hauteur = hauteur;
     }
 
+    public void positionnerUnNouveauVaisseau(int x, int y) {
+        this.vaisseau = new Vaisseau(x, y);
+    }
+
+    private char recupererMarqueDeLaPosition(int x, int y) {
+        char marque;
+        if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
+            marque = MARQUE;
+        else
+            marque = MARQUE_VIDE;
+        return marque;
+    }
+
+    private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
+        return this.aUnVaisseau() && vaisseau.occupeLaPosition(x, y);
+    }
+
+    private boolean aUnVaisseau() {
+        return vaisseau != null;
+    }
+
     @Override
     public String toString() {
         StringBuilder espaceDeJeu = new StringBuilder();
-        for (int i = 0; i < hauteur; i++) {
-            for (int j = 0; j < longueur; j++) {
-                espaceDeJeu.append('.');
+        for (int y = 0; y < hauteur; y++) {
+            for (int x = 0; x < longueur; x++) {
+                espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
             }
-            espaceDeJeu.append('\n');
+            espaceDeJeu.append(MARQUE_FIN_LIGNE);
         }
         return espaceDeJeu.toString();
     }
+
+
 }
