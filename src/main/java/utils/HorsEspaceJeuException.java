094da2d4c4ac6a6b47b58eb5c1f89509088b0fdf
Arnaud SAULOU <a.saulou@gmail.com>
2018-05-18 18:03:13 +0200

    Récupération debut debut Sprint 2

--- src/main/java/utils/HorsEspaceJeuException.java
+++ src/main/java/utils/HorsEspaceJeuException.java
@@ -3,6 +3,7 @@ package utils;
 @SuppressWarnings("serial")
 public class HorsEspaceJeuException extends RuntimeException {
 
+    //Test
     public HorsEspaceJeuException(String message) {
         super(message);
     }
