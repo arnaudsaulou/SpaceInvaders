dd68be83eac523b51bba3904429c6ef1d5895efe
thibault brissaud <thibault.brissaud@etu.unilim.fr>
2018-05-18 17:56:27 +0200

    commit antibug

--- SpaceInvader/src/main/java/SpaceInvaders.java
+++ SpaceInvader/src/main/java/SpaceInvaders.java
@@ -2,22 +2,30 @@
 public class SpaceInvaders {
 
 	  int longueur;
-	    int hauteur;
+	  int hauteur;
+	  Vaisseau vaisseau;
 
 	    public SpaceInvaders(int longueur, int hauteur) {
 		   this.longueur = longueur;
 		   this.hauteur = hauteur;
 	   }
 	    
-	 @Override
-	    public String toString() {
-	        String s = "";
-	        for (int i = 0; i < hauteur; i++) {
-	            for (int j = 0; j < longueur; j++) {
-	                s += ".";
-	            }
-	            s += "\n";
-	        }
-	        return s;
-	    }
+	    public void positionnerUnNouveauVaisseau(int x, int y) {
+	        Vaisseau vaisseau = new Vaisseau (x,y);
+		}
+	    
+	    @Override
+		public String toString() {
+			StringBuilder espaceDeJeu = new StringBuilder();
+			for (int y = 0; y < hauteur; y++) {
+				for (int x = 0; x < longueur; x++) {
+					if (vaisseau!=null && vaisseau.occupeLaPosition(x, y))
+						espaceDeJeu.append('V');
+					else
+						espaceDeJeu.append('.');
+				}
+				espaceDeJeu.append('\n');
+			}
+			return espaceDeJeu.toString();
+		}
 }
